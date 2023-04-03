3d92a48892d49844da273d8a3f2dadbd9efdaf83
Mark <mark.aaron.fisher@gmail.com>
2023-04-03 15:27:40 -0700

    follow the tutorial and flesh out the content more

---
+++ index.php
@@ -0,0 +1,76 @@
+<?php 
+// Import the Composer Autoloader to make the SDK classes accessible:
+require 'vendor/autoload.php';
+
+// Load our environment variables from the .env file:
+(Dotenv\Dotenv::createImmutable(__DIR__))->load();
+
+// Instantiate the Auth0 class with our configuration:
+$auth0 = new \Auth0\SDK\Auth0([
+	'domain' => $_ENV['AUTH0_DOMAIN'],
+	'clientId' => $_ENV['AUTH0_CLIENT_ID'],
+	'clientSecret' => $_ENV['AUTH0_CLIENT_SECRET'],
+	'cookieSecret' => $_ENV['AUTH0_COOKIE_SECRET']
+]);
+
+// Import our router library:
+use Steampixel\Route;
+
+//Define route constants:
+define('ROUTE_URL_INDEX', rtrim($_ENV['AUTH0_BASE_URL'], '/'));
+define('ROUTE_URL_LOGIN', ROUTE_URL_INDEX . '/login');
+define('ROUTE_URL_CALLBACK', ROUTE_URL_INDEX . '/callback');
+define('ROUTER_URL_LOGOUT', ROUTE_URL_INDEX . '/logout');
+
+Route::add('/', function() use ($auth0) {
+	$session = $auth0->getCredentials();
+
+	// Not logged in
+	if ($session === null) {
+		echo '<p>Please <a href="/login">log in</a>.</p>';
+		return;
+	}
+
+	// Logged in
+	echo '<pre>';
+	print_r($session->user);
+	echo '</pre>';
+
+	echo '<p>You can now <a href="/logout">log out</a>.</p>';
+
+	$emailAddress = isset($session->user['email']) ? $session->user['email'] : 'Unknown';
+	$backupName = isset($session->user['nickname']) ? $session->user['nickname'] : $emailAddress;
+	$name = isset($session-> user['name']) ? $session->user['name'] : $backupName; // @TODO use this somehow
+});
+
+	Route::add('/login', function() use($auth0) {
+		// Reset the session to avoid "invalid state" errors
+		$auth0->clear();
+
+		// set up local application session and redirect the user to Auth0 Universal Login Page
+		header("Location: " . $auth0->login(ROUTE_URL_CALLBACK));
+		exit;
+	});
+
+	Route::add('/callback', function() use ($auth0) {
+		// complete the authentication flow
+		$auth0->exchange(ROUTE_URL_CALLBACK);
+
+		//redirect back to the index route
+		header("Location: " . ROUTE_URL_INDEX);
+		exit;
+	});
+
+	Route::add('/logout', function() use ($auth0) {
+		// clear the user's local session and redirect them to the logout endpoint
+		header("Location: " . $auth0->logout(ROUTE_URL_INDEX));
+		exit;
+	});
+
+	Route::run('/');
+?>
+<p>Hi 
+    <?php
+    echo 'Mark';
+    ?>
+</p>
--- vendor/composer/autoload_static.php
+++ vendor/composer/autoload_static.php
@@ -10,10 +10,19 @@ class ComposerStaticInit06318c461a5f015baedabd7cc0469511
         '7b11c4dc42b3b3023073cb14e519683c' => __DIR__ . '/..' . '/ralouphie/getallheaders/src/getallheaders.php',
         'c964ee0ededf28c96ebd9db5099ef910' => __DIR__ . '/..' . '/guzzlehttp/promises/src/functions_include.php',
         '6e3fae29631ef280660b3cdad06f25a8' => __DIR__ . '/..' . '/symfony/deprecation-contracts/function.php',
+        '320cde22f66dd4f5d3fd621d3e88b98f' => __DIR__ . '/..' . '/symfony/polyfill-ctype/bootstrap.php',
+        '0e6d7bf4a5811bfa5cf40c5ccd6fae6a' => __DIR__ . '/..' . '/symfony/polyfill-mbstring/bootstrap.php',
+        'a4a119a56e50fbb293281d9a48007e0e' => __DIR__ . '/..' . '/symfony/polyfill-php80/bootstrap.php',
         '37a3dc5111fe8f707ab4c132ef1dbc62' => __DIR__ . '/..' . '/guzzlehttp/guzzle/src/functions_include.php',
     );
 
     public static $prefixLengthsPsr4 = array (
+        'S' => 
+        array (
+            'Symfony\\Polyfill\\Php80\\' => 23,
+            'Symfony\\Polyfill\\Mbstring\\' => 26,
+            'Symfony\\Polyfill\\Ctype\\' => 23,
+        ),
         'P' => 
         array (
             'Psr\\Log\\' => 8,
@@ -23,6 +32,7 @@ class ComposerStaticInit06318c461a5f015baedabd7cc0469511
             'Psr\\Container\\' => 14,
             'Psr\\Cache\\' => 10,
             'PsrDiscovery\\' => 13,
+            'PhpOption\\' => 10,
         ),
         'H' => 
         array (
@@ -36,6 +46,11 @@ class ComposerStaticInit06318c461a5f015baedabd7cc0469511
             'GuzzleHttp\\Psr7\\' => 16,
             'GuzzleHttp\\Promise\\' => 19,
             'GuzzleHttp\\' => 11,
+            'GrahamCampbell\\ResultType\\' => 26,
+        ),
+        'D' => 
+        array (
+            'Dotenv\\' => 7,
         ),
         'C' => 
         array (
@@ -48,6 +63,18 @@ class ComposerStaticInit06318c461a5f015baedabd7cc0469511
     );
 
     public static $prefixDirsPsr4 = array (
+        'Symfony\\Polyfill\\Php80\\' => 
+        array (
+            0 => __DIR__ . '/..' . '/symfony/polyfill-php80',
+        ),
+        'Symfony\\Polyfill\\Mbstring\\' => 
+        array (
+            0 => __DIR__ . '/..' . '/symfony/polyfill-mbstring',
+        ),
+        'Symfony\\Polyfill\\Ctype\\' => 
+        array (
+            0 => __DIR__ . '/..' . '/symfony/polyfill-ctype',
+        ),
         'Psr\\Log\\' => 
         array (
             0 => __DIR__ . '/..' . '/psr/log/src',
@@ -76,13 +103,17 @@ class ComposerStaticInit06318c461a5f015baedabd7cc0469511
         'PsrDiscovery\\' => 
         array (
             0 => __DIR__ . '/../..' . '/src',
-            1 => __DIR__ . '/..' . '/psr-discovery/discovery/src',
-            2 => __DIR__ . '/..' . '/psr-discovery/log-implementations/src',
-            3 => __DIR__ . '/..' . '/psr-discovery/http-factory-implementations/src',
-            4 => __DIR__ . '/..' . '/psr-discovery/http-client-implementations/src',
-            5 => __DIR__ . '/..' . '/psr-discovery/event-dispatcher-implementations/src',
-            6 => __DIR__ . '/..' . '/psr-discovery/container-implementations/src',
-            7 => __DIR__ . '/..' . '/psr-discovery/cache-implementations/src',
+            1 => __DIR__ . '/..' . '/psr-discovery/cache-implementations/src',
+            2 => __DIR__ . '/..' . '/psr-discovery/container-implementations/src',
+            3 => __DIR__ . '/..' . '/psr-discovery/discovery/src',
+            4 => __DIR__ . '/..' . '/psr-discovery/event-dispatcher-implementations/src',
+            5 => __DIR__ . '/..' . '/psr-discovery/http-client-implementations/src',
+            6 => __DIR__ . '/..' . '/psr-discovery/http-factory-implementations/src',
+            7 => __DIR__ . '/..' . '/psr-discovery/log-implementations/src',
+        ),
+        'PhpOption\\' => 
+        array (
+            0 => __DIR__ . '/..' . '/phpoption/phpoption/src/PhpOption',
         ),
         'Http\\Message\\MultipartStream\\' => 
         array (
@@ -112,6 +143,14 @@ class ComposerStaticInit06318c461a5f015baedabd7cc0469511
         array (
             0 => __DIR__ . '/..' . '/guzzlehttp/guzzle/src',
         ),
+        'GrahamCampbell\\ResultType\\' => 
+        array (
+            0 => __DIR__ . '/..' . '/graham-campbell/result-type/src',
+        ),
+        'Dotenv\\' => 
+        array (
+            0 => __DIR__ . '/..' . '/vlucas/phpdotenv/src',
+        ),
         'Composer\\Semver\\' => 
         array (
             0 => __DIR__ . '/..' . '/composer/semver/src',
@@ -122,8 +161,23 @@ class ComposerStaticInit06318c461a5f015baedabd7cc0469511
         ),
     );
 
+    public static $prefixesPsr0 = array (
+        'S' => 
+        array (
+            'Steampixel' => 
+            array (
+                0 => __DIR__ . '/..' . '/steampixel/simple-php-router/src',
+            ),
+        ),
+    );
+
     public static $classMap = array (
+        'Attribute' => __DIR__ . '/..' . '/symfony/polyfill-php80/Resources/stubs/Attribute.php',
         'Composer\\InstalledVersions' => __DIR__ . '/..' . '/composer/InstalledVersions.php',
+        'PhpToken' => __DIR__ . '/..' . '/symfony/polyfill-php80/Resources/stubs/PhpToken.php',
+        'Stringable' => __DIR__ . '/..' . '/symfony/polyfill-php80/Resources/stubs/Stringable.php',
+        'UnhandledMatchError' => __DIR__ . '/..' . '/symfony/polyfill-php80/Resources/stubs/UnhandledMatchError.php',
+        'ValueError' => __DIR__ . '/..' . '/symfony/polyfill-php80/Resources/stubs/ValueError.php',
     );
 
     public static function getInitializer(ClassLoader $loader)
@@ -131,6 +185,7 @@ class ComposerStaticInit06318c461a5f015baedabd7cc0469511
         return \Closure::bind(function () use ($loader) {
             $loader->prefixLengthsPsr4 = ComposerStaticInit06318c461a5f015baedabd7cc0469511::$prefixLengthsPsr4;
             $loader->prefixDirsPsr4 = ComposerStaticInit06318c461a5f015baedabd7cc0469511::$prefixDirsPsr4;
+            $loader->prefixesPsr0 = ComposerStaticInit06318c461a5f015baedabd7cc0469511::$prefixesPsr0;
             $loader->classMap = ComposerStaticInit06318c461a5f015baedabd7cc0469511::$classMap;
 
         }, null, ClassLoader::class);
